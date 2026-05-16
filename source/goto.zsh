16b1c9ba726fb014b5983740016a73055f9b3ffb
JV <jaiveer.chadda@gmail.com>
2026-05-16 17:09:08 +0100

    Reorganised the file to put the main method at the top, and clean things up a bit

--- source/goto.zsh
+++ source/goto.zsh
@@ -2,21 +2,83 @@
 
 unalias g &>/dev/null || :
 alias g=goto
+typeset -a chpwd_functions=( "${(@)chpwd_functions}" )
 
 # ——————————————————————————————————————————————————————————————————————————— #
+# —— Main Method ———————————————————————————————————————————————————————————— #
 # ——————————————————————————————————————————————————————————————————————————— #
 
-function goto::get_owner() {
-  local -ri 10 delim=$RANDOM
-  local user_info=
+# -q : no stdout
+# -Q : no stdout/stderr
+# -i : interactive
 
-  user_info="$(  stat -c "%U$delim%G" "$1" 2>/dev/null )" || \
-  user_info="$( gstat -c "%U$delim%G" "$1" 2>/dev/null )" || return 1
+function goto() {
+  setopt local_options warn_create_global
 
-  owner="${user_info%$delim*}"
-  group="${user_info#*$delim}"
+  # concatenate the input with `$IFS`, so an unquoted input like
+  #  `/path to/some dir` will be read as one argument: `"/path to/some dir"`
+  local IFS=$' \t\n\0'
+  # note: `-H` is used on vars containing paths, to hide them from `abbrpath`
+  local -rH input="${*:-"$OLDPWD"}"
+
+  # ———————————————————————————————————————————————————————————————————————— #
+
+  # if no input was passed, and `$OLDPWD` is unset/empty, then exit
+  #  and blame it on on the user for not passing an input
+  if [[ -z "$input" ]] echo "$no_input" >&2 && return 1
+
+  # ———————————————————————————————————————————————————————————————————————— #
+
+  local -H target=  # will be set by one of the sub-functions
+
+  if     [[ -d "$input" ]] { target="$input"     # dir  - just act like `cd`
+  } elif [[ -e "$input" ]] { target="$input:a:h" # file - go to the file's dir
+  } else {
+
+    local type="$( type -w "$input" )"  # outputs smth like `ls: command`
+    type="${type##*: }"  # delete until the last colon, leaving just `command`
+
+    case "$type" {
+      ( command | function ) goto::command ;;
+      ( none ) goto::error found; return 1 ;;
+      ( *    ) goto::error types; return 2 ;;
+    }
+  }
+
+  # if any of the `goto::...` functions fail, return their exit codes
+  #  they print their own error messages, so this main function doesn't have to
+  local -ri 10 exit_code=$?
+  if (( exit_code )) return exit_code
+
+  # ———————————————————————————————————————————————————————————————————————— #
+
+  ls "$target" &>/dev/null || { goto::error perms; return 1; }
+
+  # if `cd` succeeds, print the command that's about to be run
+  abbrpath -C cd "$target" >&2
+
 }
 
+# ——————————————————————————————————————————————————————————————————————————— #
+# ——— Processing Functions —————————————————————————————————————————————————— #
+# ——————————————————————————————————————————————————————————————————————————— #
+
+function goto::command() {
+  # this'll output smth like `func is a shell function from /path/to/func.zsh`
+  local -H func_path="$( whence -v "$input" 2>/dev/null )"
+
+  # if the path doesn't have a slash in it, it's not a path
+  #  (in this case, the function is usually `an autoload shell function`)
+  if (( ${#func_path/\/} == $#func_path )) { goto::error nodef; return 1; }
+
+  # strip everything until the first `/` (where the path starts)
+  # then add the `/` back
+  func_path="/${func_path#*/}"
+  target="$func_path:h"  # then get the get the [h]ead of the file
+}
+
+# ——————————————————————————————————————————————————————————————————————————— #
+# —— Error Handling ————————————————————————————————————————————————————————— #
 # ——————————————————————————————————————————————————————————————————————————— #
 
 function goto::error() {
@@ -50,91 +112,21 @@ function goto::error() {
 }
 
 # ——————————————————————————————————————————————————————————————————————————— #
+# —— Helper Functions ——————————————————————————————————————————————————————— #
 # ——————————————————————————————————————————————————————————————————————————— #
 
-function goto::directory() {
-  # if `$input` is a directory, `goto` acts pretty much exactly like `cd`
-  target="$input"
+function goto::get_owner() {
+  local -ri 10 delim=$RANDOM
+  local user_info=
+
+  user_info="$(  stat -c "%U$delim%G" "$1" 2>/dev/null )" || \
+  user_info="$( gstat -c "%U$delim%G" "$1" 2>/dev/null )" || return 1
+
+  owner="${user_info%$delim*}"
+  group="${user_info#*$delim}"
 }
 
 # ——————————————————————————————————————————————————————————————————————————— #
-
-function goto::file() {
-  # `:a` - resolve `./` and `../`
-  # `:h` - get the [h]ead of the file (eqv. to `dirname(1)`)
-  target="$input:a:h"
-}
-
-# ——————————————————————————————————————————————————————————————————————————— #
-
-function goto::command() {
-  # this'll output smth like `func is a shell function from /path/to/func.zsh`
-  local -H func_path="$( whence -v "$input" 2>/dev/null )"
-
-  # if the path doesn't have a slash in it, it's not a path
-  #  (in this case, the function is usually `an autoload shell function`)
-  if (( ${#func_path/\/} == $#func_path )) { goto::error nodef; return 1; }
-
-  # strip everything until the first `/` (where the path starts)
-  # then add the `/` back
-  func_path="/${func_path#*/}"
-  target="$func_path:h"  # then get the get the [h]ead of the file
-}
-
-# ——————————————————————————————————————————————————————————————————————————— #
-# ——————————————————————————————————————————————————————————————————————————— #
-
-# -q : no stdout
-# -Q : no stdout/stderr
-# -i : interactive
-
-function goto() {
-  setopt local_options warn_create_global
-
-  # concatenate the input with `$IFS`, so an unquoted input like
-  #  `/path to/some dir` will be read as one argument: `"/path to/some dir"`
-  local IFS=$' \t\n\0'
-  # note: `-H` is used on vars containing paths, to hide them from `abbrpath`
-  local -rH input="${*:-"$OLDPWD"}"
-
-  # ———————————————————————————————————————————————————————————————————————— #
-
-  # if no input was passed, and `$OLDPWD` is unset/empty, then exit
-  #  and blame it on on the user for not passing an input
-  if [[ -z "$input" ]] echo "$no_input" >&2 && return 1
-
-  # ———————————————————————————————————————————————————————————————————————— #
-
-  local -H target=  # will be set by one of the sub-functions
-
-  if     [[ -d "$input" ]] { goto::directory
-  } elif [[ -e "$input" ]] { goto::file
-  } else {
-
-    local type="$( type -w "$input" )"  # outputs smth like `ls: command`
-    type="${type##*: }"  # delete until the last colon, leaving `command`
-
-    case "$type" {
-      ( command | function ) goto::command ;;
-      ( none ) goto::error found; return 1 ;;
-      ( *    ) goto::error types; return 2 ;;
-    }
-  }
-
-  # if any of the `goto::...` functions fail, return their exit codes
-  #  they print their own error messages, so this main function doesn't have to
-  local -ri 10 exit_code=$?
-  if (( exit_code )) return exit_code
-
-  # ———————————————————————————————————————————————————————————————————————— #
-
-  ls "$target" &>/dev/null || { goto::error perms; return 1; }
-
-  # if `cd` succeeds, print the command that's about to be run
-  abbrpath -C cd "$target" >&2
-
-}
-
 # ——————————————————————————————————————————————————————————————————————————— #
 
 # spell:ignore nodef
